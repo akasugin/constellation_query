1953db4361905b3e99afe28de159b7504c1bcf3c
akasugin <1750087920@qq.com>
2017-12-27 11:05:56 +0800

    update

--- constellation_query/Bot.js
+++ constellation_query/Bot.js
@@ -7,18 +7,16 @@ class Bot extends BaseBot{
 
         this.addLaunchHandler(()=>{
             return {
-                outputSpeech : '欢迎查询星座!'
+                outputSpeech : '欢迎进入查询星座系统!'
             };
         });
 
         this.addIntentHandler('constellation_query', ()=>{
-            // let loc = this.getSlot('location');    
-            // let monthlySalary = this.getSlot('monthlysalary');
             let date = this.getSlot('sys.date');
             let subject = this.getSlot('query_subject');
 
             if(!date) {
-                this.nlu.ask('date');
+                this.nlu.ask('sys.date');
                 let card = new Bot.Card.TextCard('你的生日是多少呢');
 
                 // 如果有异步操作，可以返回一个promise
@@ -32,7 +30,7 @@ class Bot extends BaseBot{
 
             if(!subject) {
                 let card = new Bot.Card.TextCard('你想查询星座么');
-                this.nlu.ask('location');
+                this.nlu.ask('query_subject');
                 return {
                     card : card,
                     outputSpeech : '你想查询星座么'
@@ -40,6 +38,13 @@ class Bot extends BaseBot{
             }
 
             if(date&&subject){
+                console.log(date);
+                function toAstro(cMonth,cDay) {
+                    var s   = "\u9b54\u7faf\u6c34\u74f6\u53cc\u9c7c\u767d\u7f8a\u91d1\u725b\u53cc\u5b50\u5de8\u87f9\u72ee\u5b50\u5904\u5973\u5929\u79e4\u5929\u874e\u5c04\u624b\u9b54\u7faf";
+                    var arr = [20,19,21,21,21,22,23,23,23,23,22,22];//1月到12月
+                    return s.substr(cMonth*2 - (cDay < arr[cMonth-1] ? 2 : 0),2) + "\u5ea7";//座
+                }
+
                 return {
                     outputSpeech : '水瓶座'
                 };
